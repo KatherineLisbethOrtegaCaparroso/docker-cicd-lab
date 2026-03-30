25cdb68c4bddb66eb216e6bfda4a9c3ea22b41ed
Katherine Lisbeth Ortega Caparroso <katherine.ortega@uip.edu.pa>
2026-03-30 13:57:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hola CI/CD funcionando🚀"
+    return "Hola pipeline final🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
